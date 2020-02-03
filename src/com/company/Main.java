240b378ab388875ed1f078b622b969b85dd24eef
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-02-03 13:21:38 +1300

    Operator Challenge working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double x = 20.00;
+        double y = 80.00;
+
+        double total = (x + y) * 100;
+
+        double remainder = total % 40.00;
+
+        boolean isZero = (remainder == 0) ? true : false;
+
+        if (isZero) {
+            System.out.println("The remainder is zero");
+        }
     }
 }
